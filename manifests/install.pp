e3270d4d9797485d10b041bf9bc29d9f74c34034
tduran <tduran@godaddy.com>
2014-09-19 09:41:22 -0700

    Merge with upstream changes

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,10 +4,20 @@ class uchiwa::install {
   case $::osfamily {
     'Debian': {
       class { 'uchiwa::repo::apt': }
+      if ($uchiwa::install_repo) {
+        $repo_require = Apt::Source['sensu']
+      } else {
+        $repo_require = undef
+      }
     }
 
     'RedHat': {
       class { 'uchiwa::repo::yum': }
+      if ($uchiwa::install_repo) {
+        $repo_require = Yumrepo['sensu']
+      } else {
+        $repo_require = undef
+      }
     }
 
     default: { alert("${::osfamily} not supported yet") }
@@ -29,7 +39,8 @@ class uchiwa::install {
   }
 
   package { $uchiwa::package_name:
-    ensure => $uchiwa::version,
+    ensure  => $uchiwa::version,
+    require => $repo_require,
   }
 
 }
